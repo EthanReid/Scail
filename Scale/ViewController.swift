ca9fa3c2b74363689d61afab97f6696c8a8c8fb2
Ethan Reid <erpreid@gmail.com>
2015-11-14 11:25:58 -0800

    scheme

---
+++ Scale/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  Scale
+//
+//  Created by Ethan Reid on 013/11/15.
+//  Copyright © 2015 Ethan Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var forceLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            if #available(iOS 9.0, *) {
+                if traitCollection.forceTouchCapability == UIForceTouchCapability.Available {
+                    if touch.force >= touch.maximumPossibleForce {
+                        forceLabel.text = "385+ grams"
+                    } else {
+                        let force = touch.force/touch.maximumPossibleForce
+                        let grams = force * 385
+                        let roundGrams = Int(grams)
+                        forceLabel.text = "\(roundGrams) grams"
+                    }
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        forceLabel.text = "0 gram"
+    }
+}
+
